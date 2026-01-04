9de4ca5cea506ea87ed93a79e5aa9c1d36ee1ada
Aswin S <aswinsatheesh03@gmail.com>
2026-01-04 17:33:06 +0530

    Added First Model (Asset)

---
+++ backend/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Asset(Base):
+    __tablename__ = "assets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    asset_name = Column(String, nullable=False)
+    asset_type = Column(String, nullable=False)
+    serial_number = Column(String, unique=True, nullable=False)
+    status = Column(String, default="Available")
