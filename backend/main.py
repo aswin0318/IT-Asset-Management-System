7a7ebe459fbf9cde8f0d9021773ae54113bb5ac5
Aswin S <aswinsatheesh03@gmail.com>
2026-01-05 02:27:07 +0530

    First API Endpoint Created

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import models
-from .database import engine
-
-print(">>> MAIN.PY LOADED")
-print(">>> MODELS:", models.Base.metadata.tables.keys())
+from .database import engine, get_db
+from .schemas import AssetCreate, AssetResponse
+from sqlalchemy.orm import Session
 
 app = FastAPI(title="IT Asset Management System")
 models.Base.metadata.create_all(bind=engine)
+
+@app.post("/assets", response_model=AssetResponse)
+def create_asset(asset: AssetCreate, db:Session = Depends(get_db)):
+    new_asset = models.Asset(
+        asset_name = asset.asset_name,
+        asset_type = asset.asset_type,
+        serial_number = asset.serial_number
+    )
+    db.add(new_asset)
+    db.commit()
+    db.refresh(new_asset)
+    return new_asset
 
 @app.get("/")
 def root():
---
+++ backend/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class AssetCreate(BaseModel):
+    asset_name: str
+    asset_type: str
+    serial_number: str
+
+class AssetResponse(BaseModel):
+    id: int
+    asset_name: str
+    asset_type: str
+    serial_number: str
+    status: str
+
+    class Config:
+        orm_mode = True
